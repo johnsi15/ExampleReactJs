b19d9240e335f130ea57b0abdea47088552d554e
John serrano <andrey_201015@hotmail.com>
2017-04-26 18:07:05 -0500

    Configurando webpack me falta que funcione el server

---
+++ source/server.js
@@ -0,0 +1,17 @@
+import http from 'http';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+
+
+function requestHandler(request, response){
+  const html = renderToString(
+    React.DOM.h1(null, 'Hola mundo')
+  );
+
+  response.write(html);
+  response.end();
+
+  const server = http.createServer(requestHandler);
+
+  server.listen(3000);
+}
---
+++ webpack/webpack.server.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: './source/server.js',
+  output: {
+    filename: 'index.js',
+    path: __dirname +'built/server/',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: 'json',
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules)/,
+        query: {
+          presets: ['latest-minimal', 'react']
+        }
+      }
+    ]
+  },
+  target: 'node',
+};
